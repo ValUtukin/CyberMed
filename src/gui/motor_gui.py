a96216d4df28ddcf70325aee2c269f8508674f29
Valery <swimingval@gmail.com>
2023-01-26 18:59:36 +0700

    Bytestring input

--- src/gui/motor_gui.py
+++ src/gui/motor_gui.py
@@ -9,20 +9,40 @@ def right_rotate():
     print('Rotate right')
 
 
+def get_bytestring():
+    bytestring = str(bytestring_entry.get())
+    warning_label = Label(root, text='8 bits!', background='#f65e05', font=('Arial', 10,))
+    if len(bytestring) == 8:
+        warning_label["text"] = ''
+        warning_label["background"] = '#0585e8'
+        warning_label.place(relx=0.81, rely=0.01, relheight=0.056, relwidth=0.15)
+        print(f'We got nice byte => {bytestring}')
+    else:
+        warning_label.place(relx=0.81, rely=0.01, relheight=0.056, relwidth=0.15)
+
+
 root = Tk()
 
-root['bg'] = '#ffffff'
+root['bg'] = '#0585e8'
 root.title('Motor Client')
 root.geometry('480x320')
 photo = PhotoImage(file="../../Images/cyber_hand.png")
 root.iconphoto(False, photo)
 root.resizable(width=False, height=True)
 
-left_rotate_button = Button(root, text='Rotate left', command=left_rotate)
-left_rotate_button.place(relx=0.05, rely=0.15, relheight=0.1)
+# left_rotate_button = Button(root, text='Rotate left', command=left_rotate)
+# left_rotate_button.place(relx=0.05, rely=0.15, relheight=0.1)
+#
+# right_rotate_button = Button(root, text='Rotate right', command=right_rotate)
+# right_rotate_button.place(relx=0.2, rely=0.15, relheight=0.1)
 
-right_rotate_button = Button(root, text='Rotate right', command=right_rotate)
-right_rotate_button.place(relx=0.2, rely=0.15, relheight=0.1)
+bytestring_label = Label(root, text='Enter byte string e.g. "00110110" (7 <= 0)', background='#b8dffe', font=('Arial', 10))
+bytestring_label.place(relx=0.01, rely=0.01, relheight=0.05)
 
+bytestring_entry = Entry()
+bytestring_entry.place(relx=0.54, rely=0.01, relheight=0.05, relwidth=0.15)
+
+bytestring_button = Button(root, text='Send', command=get_bytestring)
+bytestring_button.place(relx=0.7, rely=0.01, relheight=0.056, relwidth=0.1)
 
 root.mainloop()
