4df6322ea1430bd7d220017ea886390279cef80b
Valery <swimingval@gmail.com>
2023-05-12 16:40:00 +0700

    Lower frame update

--- src/LowerMotorFrame.py
+++ src/LowerMotorFrame.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from pubsub import pub
 
 
-class UpperMotorFrame:
+class LowerMotorFrame:
     def __init__(self, parent, identifier):
         self.container = parent
         self.identifier = identifier
@@ -14,7 +14,7 @@ class UpperMotorFrame:
         self.power_checkbox = Checkbutton(self.main_frame, text="Power", background='#01B6CF',
                                           command=self.power_switch,
                                           variable=self.power_status, onvalue=True, offvalue=False)
-        self.motor1_label = Label(self.main_frame, text='Pinky', background='#b8dffe', font=('Arial', 10))
+        self.motor1_label = Label(self.main_frame, text='Thumb', background='#b8dffe', font=('Arial', 10))
         self.motor1_left_button = Button(self.main_frame, text='Compress', command=self.motor1_rotate_left)
         self.motor1_right_button = Button(self.main_frame, text='Release', command=self.motor1_rotate_right)
         self.motor1_stop_button = Button(self.main_frame, text='Stop', command=self.motor1_rotate_stop)
@@ -35,7 +35,7 @@ class UpperMotorFrame:
                                                command=self.motor1_adc_change, variable=self.motor1_adc_status,
                                                onvalue=True, offvalue=False)
 
-        self.motor2_label = Label(self.main_frame, text='Thumb', background='#b8dffe', font=('Arial', 10))
+        self.motor2_label = Label(self.main_frame, text='Pinky', background='#b8dffe', font=('Arial', 10))
         self.motor2_left_button = Button(self.main_frame, text='Compress', command=self.motor2_rotate_left)
         self.motor2_right_button = Button(self.main_frame, text='Release', command=self.motor2_rotate_right)
         self.motor2_stop_button = Button(self.main_frame, text='Stop', command=self.motor2_rotate_stop)
@@ -77,7 +77,7 @@ class UpperMotorFrame:
                                                command=self.motor3_adc_change, variable=self.motor3_adc_status,
                                                onvalue=True, offvalue=False)
 
-        self.motor4_label = Label(self.main_frame, text='Index', background='#b8dffe', font=('Arial', 10))
+        self.motor4_label = Label(self.main_frame, text='Ring', background='#b8dffe', font=('Arial', 10))
         self.motor4_left_button = Button(self.main_frame, text='Compress', command=self.motor4_rotate_left)
         self.motor4_right_button = Button(self.main_frame, text='Release', command=self.motor4_rotate_right)
         self.motor4_stop_button = Button(self.main_frame, text='Stop', command=self.motor4_rotate_stop)
@@ -98,7 +98,7 @@ class UpperMotorFrame:
                                                command=self.motor4_adc_change, variable=self.motor4_adc_status,
                                                onvalue=True, offvalue=False)
 
-        self.motor5_label = Label(self.main_frame, text='Ring', background='#b8dffe', font=('Arial', 10))
+        self.motor5_label = Label(self.main_frame, text='Index', background='#b8dffe', font=('Arial', 10))
         self.motor5_left_button = Button(self.main_frame, text='Compress', command=self.motor5_rotate_left)
         self.motor5_right_button = Button(self.main_frame, text='Release', command=self.motor5_rotate_right)
         self.motor5_stop_button = Button(self.main_frame, text='Stop', command=self.motor5_rotate_stop)
@@ -123,29 +123,29 @@ class UpperMotorFrame:
         self.main_label.place(relx=0.4, rely=0.01)
         self.power_checkbox.place(relx=0.005, rely=0.01, relheight=0.07, relwidth=0.1)
 
-        self.motor2_label.place(relx=0.005, rely=0.16, relheight=0.03)
-        self.motor2_left_button.place(relx=0.09, rely=0.14, relheight=0.08, relwidth=0.11)
-        self.motor2_right_button.place(relx=0.205, rely=0.14, relheight=0.08, relwidth=0.09)
-        self.motor2_stop_button.place(relx=0.3, rely=0.14, relheight=0.08, relwidth=0.1)
-        self.motor2_pwm_scale.place(relx=0.405, rely=0.11)
-        self.motor2_timer_frame.place(relx=0.59, rely=0.11, relheight=0.159, relwidth=0.3)
-        self.motor2_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
-        self.motor2_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
-        self.motor2_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
-        self.motor2_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
-        self.motor2_adc_checkbox.place(relx=0.895, rely=0.14)
+        self.motor1_label.place(relx=0.005, rely=0.16, relheight=0.03)
+        self.motor1_left_button.place(relx=0.09, rely=0.14, relheight=0.08, relwidth=0.11)
+        self.motor1_right_button.place(relx=0.205, rely=0.14, relheight=0.08, relwidth=0.09)
+        self.motor1_stop_button.place(relx=0.3, rely=0.14, relheight=0.08, relwidth=0.1)
+        self.motor1_pwm_scale.place(relx=0.405, rely=0.11)
+        self.motor1_timer_frame.place(relx=0.59, rely=0.11, relheight=0.159, relwidth=0.3)
+        self.motor1_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
+        self.motor1_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
+        self.motor1_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
+        self.motor1_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
+        self.motor1_adc_checkbox.place(relx=0.895, rely=0.14)
 
-        self.motor4_label.place(relx=0.005, rely=0.335, relheight=0.03)
-        self.motor4_left_button.place(relx=0.09, rely=0.31, relheight=0.08, relwidth=0.11)
-        self.motor4_right_button.place(relx=0.205, rely=0.31, relheight=0.08, relwidth=0.09)
-        self.motor4_stop_button.place(relx=0.3, rely=0.31, relheight=0.08, relwidth=0.1)
-        self.motor4_pwm_scale.place(relx=0.405, rely=0.28)
-        self.motor4_timer_frame.place(relx=0.59, rely=0.275, relheight=0.159, relwidth=0.3)
-        self.motor4_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
-        self.motor4_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
-        self.motor4_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
-        self.motor4_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
-        self.motor4_adc_checkbox.place(relx=0.895, rely=0.32)
+        self.motor5_label.place(relx=0.005, rely=0.325, relheight=0.03)
+        self.motor5_left_button.place(relx=0.09, rely=0.315, relheight=0.08, relwidth=0.11)
+        self.motor5_right_button.place(relx=0.205, rely=0.315, relheight=0.08, relwidth=0.09)
+        self.motor5_stop_button.place(relx=0.3, rely=0.315, relheight=0.08, relwidth=0.1)
+        self.motor5_pwm_scale.place(relx=0.405, rely=0.28)
+        self.motor5_timer_frame.place(relx=0.59, rely=0.275, relheight=0.159, relwidth=0.3)
+        self.motor5_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
+        self.motor5_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
+        self.motor5_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
+        self.motor5_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
+        self.motor5_adc_checkbox.place(relx=0.895, rely=0.32)
 
         self.motor3_label.place(relx=0.005, rely=0.51, relheight=0.03)
         self.motor3_left_button.place(relx=0.09, rely=0.485, relheight=0.08, relwidth=0.11)
@@ -159,29 +159,29 @@ class UpperMotorFrame:
         self.motor3_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
         self.motor3_adc_checkbox.place(relx=0.895, rely=0.5)
 
-        self.motor5_label.place(relx=0.005, rely=0.665, relheight=0.05)
-        self.motor5_left_button.place(relx=0.09, rely=0.66, relheight=0.08, relwidth=0.11)
-        self.motor5_right_button.place(relx=0.205, rely=0.66, relheight=0.08, relwidth=0.09)
-        self.motor5_stop_button.place(relx=0.3, rely=0.66, relheight=0.08, relwidth=0.1)
-        self.motor5_pwm_scale.place(relx=0.405, rely=0.62)
-        self.motor5_timer_frame.place(relx=0.59, rely=0.615, relheight=0.159, relwidth=0.3)
-        self.motor5_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
-        self.motor5_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
-        self.motor5_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
-        self.motor5_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
-        self.motor5_adc_checkbox.place(relx=0.895, rely=0.66)
+        self.motor4_label.place(relx=0.005, rely=0.67, relheight=0.05)
+        self.motor4_left_button.place(relx=0.09, rely=0.66, relheight=0.08, relwidth=0.11)
+        self.motor4_right_button.place(relx=0.205, rely=0.66, relheight=0.08, relwidth=0.09)
+        self.motor4_stop_button.place(relx=0.3, rely=0.66, relheight=0.08, relwidth=0.1)
+        self.motor4_pwm_scale.place(relx=0.405, rely=0.62)
+        self.motor4_timer_frame.place(relx=0.59, rely=0.615, relheight=0.159, relwidth=0.3)
+        self.motor4_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
+        self.motor4_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
+        self.motor4_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
+        self.motor4_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
+        self.motor4_adc_checkbox.place(relx=0.895, rely=0.66)
 
-        self.motor1_label.place(relx=0.005, rely=0.84, relheight=0.05)
-        self.motor1_left_button.place(relx=0.09, rely=0.83, relheight=0.08, relwidth=0.11)
-        self.motor1_right_button.place(relx=0.205, rely=0.83, relheight=0.08, relwidth=0.09)
-        self.motor1_stop_button.place(relx=0.3, rely=0.83, relheight=0.08, relwidth=0.1)
-        self.motor1_pwm_scale.place(relx=0.405, rely=0.795)
-        self.motor1_timer_frame.place(relx=0.59, rely=0.785, relheight=0.159, relwidth=0.3)
-        self.motor1_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
-        self.motor1_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
-        self.motor1_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
-        self.motor1_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
-        self.motor1_adc_checkbox.place(relx=0.895, rely=0.82)
+        self.motor2_label.place(relx=0.005, rely=0.84, relheight=0.05)
+        self.motor2_left_button.place(relx=0.09, rely=0.83, relheight=0.08, relwidth=0.11)
+        self.motor2_right_button.place(relx=0.205, rely=0.83, relheight=0.08, relwidth=0.09)
+        self.motor2_stop_button.place(relx=0.3, rely=0.83, relheight=0.08, relwidth=0.1)
+        self.motor2_pwm_scale.place(relx=0.405, rely=0.795)
+        self.motor2_timer_frame.place(relx=0.59, rely=0.785, relheight=0.159, relwidth=0.3)
+        self.motor2_timer_label.place(relx=0.05, rely=0.05, relheight=0.25, relwidth=0.9)
+        self.motor2_timer_entry.place(relx=0.2, rely=0.5, relheight=0.35, relwidth=0.35)
+        self.motor2_timer_sec_label.place(relx=0.6, rely=0.52, relheight=0.21, relwidth=0.15)
+        self.motor2_timer_checkbox.place(relx=0.05, rely=0.5, relheight=0.3, relwidth=0.13)
+        self.motor2_adc_checkbox.place(relx=0.895, rely=0.82)
 
     def switch_button_color(self, button, color='#19e676'):
         if button['bg'] == 'SystemButtonFace':
@@ -287,7 +287,7 @@ if __name__ == '__main__':
     root.iconphoto(False, photo)
     root.resizable(width=False, height=False)
 
-    frame = UpperMotorFrame(root, 'Upper')
+    frame = LowerMotorFrame(root, 'Lower')
     frame.main_frame.place(relx=0.01, rely=0.01, height=340, width=600)
     frame.setup_layout()
 
